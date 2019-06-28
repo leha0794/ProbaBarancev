f2a670cd3c3e180ba8f84e2bead0646ce463f6cd
leha0794 <leha0794@mail.ru>
2019-06-28 17:23:03 +0300

    lesson 12

--- conftest.py
+++ conftest.py
@@ -1,9 +1,20 @@
 import pytest
 from fixture.application import Application
 
-# @pytest.fixture(scope="session") // если нужно все тесты делать не закрывая браузер, тоесть за одну сессию
-@pytest.fixture
+
 def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
+
+
+
+# @pytest.fixture(scope="session") // если нужно все тесты делать не закрывая браузер, тоесть за одну сессию, создается только одна фикстура
+# def app(request):
+#     fixture = Application()
+#     fixture.session.login(Logi(email="leha0793@mail.ru", password="qwerty12345")) // если все делается в одном логине
+#     def fin():
+#         fixture.session.logout()
+#         fixture.destroy()
+#     request.addfinalizer(fin)
+#     return fixture
